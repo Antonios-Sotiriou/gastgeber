a671949ae3a5250ee8eed0da138efd8df621476d
Antonios Sotiriou <antonios.sotiriou@hotmail.gr>
2021-09-03 01:27:18 +0300

    Unimportant improvements 1.

--- gastgeber.c
+++ gastgeber.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <string.h>
 
 struct Room {
     int id;
     char type[20];
     int guest_id;
     bool reserved;
+};
+struct Customer {
+    int id;
+    char first_name[30];
+    char last_name[30];
+    char nationality[30];
+    bool stammgast;
 };
 
 void reserve();
@@ -15,8 +23,8 @@ void displayAll();
 void modify();
 void clearRes();
 void search();
-/* Function to keep the program runing */
-char mygetch();
+void displayMainLogo();
+void displayRoomInfoLogo();
 
 char roomsdb[] = {"/home/as/gastgeber/data/roomsdb.dat"};
 
@@ -41,10 +49,11 @@ void main() {
             if(feof(fp)) {
                 break;
             }
-            printf("Room ID: %d\n", room.id);
-            printf("Room Type: %s\n", room.type);
-            printf("Guest ID: %d\n", room.guest_id);
-            printf("Room Reserved: %s\n", room.reserved ? "true" : "false");
+            printf(" --------------------------------------------------------\n");
+            printf("|  Room ID  |  Room Type  |  Guest ID  |  Room Reserved  |\n");
+            printf(" --------------------------------------------------------\n");
+            printf("|    %d     |    %s       |    %d      |       %s        |\n", room.id, room.type, room.guest_id, room.reserved ? "true" : "false");
+            printf(" --------------------------------------------------------\n");
         }
         fclose(fp);
     }
@@ -52,28 +61,17 @@ void main() {
     int choice;
 
     while(1) {
-        //system("clear");
-        printf("*****************************************************\n");
-        printf("*                  Welcome to Vesta                 *\n");
-        printf("*       The most Advanced and client oriented       *\n");
-        printf("*             Hotel Managment Software              *\n");
-        printf("*                                                   *\n");
-        printf("*****************************************************\n\n");
-        printf("Choose your action...\n\n");
-        printf("1. Reserve a room\n");
-        printf("2. Display room info\n");
-        printf("3. Display all rooms\n");
-        printf("4. Modify a room\n");
-        printf("5. Clear room Reservation\n");
-        printf("6. Search\n");
-        printf("0. Exit\n\n");
 
+        displayMainLogo();
         scanf("%d", &choice);
 
         switch(choice) {
             case 1 : reserve();
                 break;
-            case 0 : exit(0);
+            case 2 : display();
+                break;
+            case 0 : system("clear");
+                exit(0);
                 break;
             default :
                 break;
@@ -83,52 +81,85 @@ void main() {
 }
 
 void reserve() {
+    system("clear");
+    printf("Reserve room...\n");
+}
+
+void display() {
 
     struct Room room;
     FILE *fp;
     fp = fopen(roomsdb, "rb");
 
-    int room_id, found = 0;
+    displayRoomInfoLogo();
 
-    system("clear");
-    printf("*************************************\n");
-    printf("*       Hotel Room reservation.     *\n");
-    printf("*************************************\n\n");
+    char room_id[63]; 
+    int found = 0, check = 0;
+    char c; 
+    while(c = getc(stdin) != '\n' && c != '\t');
     
-    while(1) {
-        printf("Enter Room id:\n");  
-        int test = scanf("%d", &room_id);
+    while(check == 0) {
+    
+        printf("Enter Room ID(1-99):\n");  
+        fgets(room_id, 63, stdin);
         
-        if(test == 0) {
+        if (strlen(room_id) < 1 || strlen(room_id) > 4) {
+            check = 0;
+            displayRoomInfoLogo();
             printf("Invalid Room ID.\n");
-            scanf("%*[^\n]");
-            printf("Enter again a correct Room ID:\n");
-            test = scanf("%d", &room_id);
+            continue;
+        } else if (sscanf(room_id, "%d", &check) != 1) {
+            check = 0;
+            displayRoomInfoLogo();
+            printf("Invalid Room ID.Provide a number.\n");
+            continue;
+        } else if (check < 1 || check > 100) {
+            check = 0;
+            displayRoomInfoLogo();
+            printf("Room ID's range is 1 to 99.\n");
+            continue;
         } else {
             while(1) {
                 fread(&room, sizeof(room), 1, fp);
                 if(feof(fp)) {
                     break;
                 }
-                if(room.id == room_id) {
-                    found = 1;
-                    printf("Room ID: %d\n", room.id);
-                    printf("Room Type: %s\n", room.type);
-                    printf("Guest ID: %d\n", room.guest_id);
-                    printf("Room Reserved: %s\n", room.reserved ? "true" : "false");
-                    break;
+                if(room.id == check) {
+                    printf(" ------------------------------------------------------------------------------\n");
+                    printf("|Room ID            |Room Type          |Guest ID           |Room Reserved     |\n");
+                    printf(" ------------------------------------------------------------------------------\n");
+                    printf("|%d                 |%s                 |%d                 |%s                |\n", room.id, room.type, room.guest_id, room.reserved ? "true" : "false");
+                    printf(" ------------------------------------------------------------------------------\n");
                 }
             }
-            if(found == 0) {
-                printf("No Room found with the given ID.\n");
-            }
         }
-        getc(stdin);
     }
     fclose(fp);
     printf("Press Enter to continue...\n");
+}
 
-    int next = getc(stdin);
-    printf("Next = %d", next);
+/* Display functions for logos and signs. */
+void displayMainLogo() {
+    //system("clear");
+    printf("*****************************************************\n");
+    printf("*                  Welcome to Vesta                 *\n");
+    printf("*       The most Advanced and client oriented       *\n");
+    printf("*             Hotel Managment Software              *\n");
+    printf("*                                                   *\n");
+    printf("*****************************************************\n\n");
+    printf("Choose your action...\n\n");
+    printf("1. Reserve a room\n");
+    printf("2. Display room info\n");
+    printf("3. Display all rooms\n");
+    printf("4. Modify a room\n");
+    printf("5. Clear room Reservation\n");
+    printf("6. Search\n");
+    printf("0. Exit\n\n");
+}
+void displayRoomInfoLogo() {
+        system("clear");
+        printf("*************************************\n");
+        printf("*    Display Room Informations.     *\n");
+        printf("*************************************\n\n");
 }
 
