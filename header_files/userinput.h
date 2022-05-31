08a9a76d21901fc97b1e96ce6473641039bfb8b8
Antonios Sotiriou <antonios.sotiriou@hotmail.gr>
2022-05-31 17:16:37 +0300

    Fixed a buffer overflow bugg in getformatedDate and in getTimelessDate functions.

--- header_files/userinput.h
+++ header_files/userinput.h
@@ -5,6 +5,8 @@ void buffer_clear();
 int getnuminput(int max_len, bool spaces);
 float getfloat(int max_len);
 int getformatedDate(char *room_date);
+int getDate(int max_len, char *input);
+int getTimelessDate(char *room_date);
 char* getString(int str_len);
 char* getSpString(int str_len);
 
